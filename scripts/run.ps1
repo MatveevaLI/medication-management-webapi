ad4151fff099acfdf52a277de6f29021fa1d368c
MatveevaLI <ladaivanna.matveeva@gmail.com>
2024-04-20 23:24:09 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -32,6 +32,9 @@ switch ($command) {
     "mongo" {
         mongo up
     }
+    "docker" {
+        docker build -t matveevali/medication-management-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     default {
         throw "Unknown command: $command"
     }
